5f2f46610d2d4887d8fafa3c8be4d7ab2c9a4f4f
lucky <luckyverma14082004@gmail.com>
2025-12-24 19:49:37 +0530

    updated

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -49,7 +49,13 @@ userSchema.methods.comparePassword=async function (password){
 }
 
 
-
+userSchema.methods.generateToken =  async function(){
+ try {
+    return jwt.sign({
+        userId:this._id.toString (),
+        email:this.email,
+        isAdmin:this.isAdmin,
+    },
   process.env.JWT_KEY, {
     expiresIn:"30d",
   } 
