d26e2137a66119a9b7fc1440a743ac84955a0419
lucky <luckyverma14082004@gmail.com>
2025-12-26 20:31:12 +0530

    error resolved

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -48,7 +48,18 @@ userSchema.methods.comparePassword=async function (password){
     
 }
 
+userSchema.methods.generateToken =  async function(){
+ try {
+    return jwt.sign({
+        userId:this._id.toString (),
+        email:this.email,
+        isAdmin:this.isAdmin,
+    },
 
+  process.env.JWT_KEY, {
+    expiresIn:"30d",
+  } 
+);
     
  } catch (error) {
     console.error(error);
